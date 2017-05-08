8381d9b0bf5ae2b6b52f8166408065574d155e78
justinhessdev <justinhessdev@gmail.com>
2017-05-08 10:51:37 -0400

    new email for auth

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const
     express = require('express'),
     app = express(),
     logger = require('morgan'),
-    bodyParser = require('body-parser')
+    bodyParser = require('body-parser'),
+    nodemailer = require('nodemailer')
 
 // environment port
 const port = process.env.PORT || 3000
@@ -17,6 +18,67 @@ app.get('/', (req, res) => {
   res.sendFile("./index.html", {root: __dirname})
 })
 
+app.post('/contact', (req, res) => {
+  console.log("The request body is: ");
+  console.log(req.body);
+
+  // if(req.body.company) {
+  //   res.render('contact', {
+  //     title: 'Contact',
+  //     err: true,
+  //     page: 'contact',
+  //     type: 'empty',
+  //     body: req.body.message,
+  //     name: req.body.name,
+  //     email: req.body.email,
+  //     msg: 'Spam detected',
+  //     description: 'spam'});
+  //   return;
+  // }
+
+  // if(! req.body.name || ! req.body.email || ! req.body.message) {
+  //   res.render('contact', {
+  //     title: 'Contact',
+  //     err: true,
+  //     page: 'contact',
+  //     type: 'empty',
+  //     body: req.body.message,
+  //     name: req.body.name,
+  //     email: req.body.email,
+  //     msg: 'fill in info',
+  //     description: 'infooo'});
+  //   return;
+  // }
+
+  var transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+          user: 'winitdevproject@gmail.com', // Your email id
+          pass: 'winit12345' // Your password
+      }
+  });
+
+  var mailOptions = {
+      from: req.body.email,
+      to: 'justinhessdev@gmail.com',
+      subject:'Winit Project -- Reaching out',
+      text: 'Email: ' + req.body.email + '\nPhone: ' + req.body.phone + '\nMessage: ' + req.body.message
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+      if(error){
+          console.log(error);
+          res.json({failure: 'error'});
+      }else{
+          console.log('Message sent: ' + info.response);
+          res.json({
+            "success": 'Your message was sent',
+            "next steps": "I will respond to your email shortly"
+          });
+      };
+  });
+});
+
 app.listen(port, (err) => {
   console.log(err || 'listening on port ' + port)
-})
+});
