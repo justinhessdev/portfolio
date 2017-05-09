96e9fe3c0f3fd122932496f61e8bd473b11aebb0
justinhessdev <justinhessdev@gmail.com>
2017-05-09 02:55:17 -0400

    linter updates

--- app.js
+++ app.js
@@ -1,73 +1,72 @@
-const
-    dotenv = require('dotenv').load({silent: true}),
-    express = require('express'),
-    app = express(),
-    mongoose = require('mongoose'),
-    logger = require('morgan'),
-    bodyParser = require('body-parser'),
-    nodemailer = require('nodemailer'),
-    ejs = require('ejs'),
-    ejsLayouts = require('express-ejs-layouts'),
-    methodOverride = require('method-override'),
-    jwt = require('jsonwebtoken'),
-    Bitly = require('bitly'),
-    bitly = new Bitly('b4ec80a1062537de1006970c9b65eb1217cb624d');
-    port = (process.env.PORT || 3000),
-    mongoConnectionString = (process.env.MONGODB_URL || 'mongodb://localhost/winit-app'),
-    messageRoutes = require('./routes/messages.js'),
-    Message = require('./models/Message.js'),
-    Token = require('./models/Token.js')
+/* eslint one-var: ["error", "always"]*/
+/* eslint-env es6*/
 
-console.log("The mongodb url is: ");
-console.log(process.env.MONGODB_URL);
+const
+  dotenv = require('dotenv').load({ silent: true }),
+  express = require('express'),
+  app = express(),
+  mongoose = require('mongoose'),
+  logger = require('morgan'),
+  path = require('path'),
+  bodyParser = require('body-parser'),
+  nodemailer = require('nodemailer'),
+  ejs = require('ejs'),
+  ejsLayouts = require('express-ejs-layouts'),
+  methodOverride = require('method-override'),
+  jwt = require('jsonwebtoken'),
+  Bitly = require('bitly'),
+  bitly = new Bitly('b4ec80a1062537de1006970c9b65eb1217cb624d'),
+  port = (process.env.PORT || 3000),
+  mongoConnectionString = (process.env.MONGODB_URL || 'mongodb://localhost/winit-app'),
+  messageRoutes = require('./routes/messages.js'),
+  Message = require('./models/Message.js');
 
 // mongoose connection
 mongoose.connect(mongoConnectionString, (err) => {
-  console.log(err || "Connected to MongoDB (winit-app)")
-})
+  console.log(err || 'Connected to MongoDB (winit-app)');
+});
 
-app.use(methodOverride('_method'))
-app.set('view engine', 'ejs')
-app.use(ejsLayouts)
-app.use(express.static(__dirname + '/public'))
-app.use(logger('dev'))
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
+app.use(methodOverride('_method'));
+app.set('view engine', 'ejs');
+app.use(ejsLayouts);
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.use('/messages', messageRoutes)
+app.use('/messages', messageRoutes);
 
 // send res (response) back to client when client makes get request at root.
 // response contains sendFile of our client index.html so client/chrome will know to populate page
 app.get('/', (req, res) => {
-  res.render('index')
-})
+  res.render('index');
+});
 
-app.post('/contact' , (req, res) => {
-  var newMessage = new Message(req.body)
+app.post('/contact', (req, res) => {
+  const newMessage = new Message(req.body);
   newMessage.save((err, message) => {
-    if(err) console.log(err)
+    if (err) console.log(err);
     // sign synchronously
-    var genToken = jwt.sign({ messageId: message._id }, 'shhhhh');
-    var genURL = "https://winit-app.herokuapp.com/token/"+genToken;
+    const
+      genToken = jwt.sign({ messageId: message._id }, 'shhhhh'),
+      genURL = `https://winit-app.herokuapp.com/token/${genToken}`;
     bitly.shorten(genURL)
-    .then(function(response) {
-      var short_url = response.data.url
-      res.render('token', {short_url})
-    }, function(error) {
-      throw error;
+    .then((response) => {
+      const shortUrl = response.data.url;
+      res.render('token', { shortUrl });
     });
-  })
-})
-
-app.get('/token/:id' , (req, res) => {
-    // verify a token symmetric - synchronous
-    var decoded = jwt.verify(req.params.id, 'shhhhh');
-    Message.findById(decoded.messageId, (err, message) => {
-      if (err) res.json({error: "401"})
-      res.render('message', {message})
-    })
   });
+});
+
+app.get('/token/:id', (req, res) => {
+    // verify a token symmetric - synchronous
+  const decoded = jwt.verify(req.params.id, 'shhhhh');
+  Message.findById(decoded.messageId, (err, message) => {
+    if (err) res.json({ error: '401' });
+    res.render('message', { message });
+  });
+});
 
 app.listen(port, (err) => {
-  console.log(err || 'listening on port ' + port)
+  console.log(err || 'listening on port ' + port);
 });
